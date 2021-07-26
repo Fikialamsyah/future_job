ed83f96561b6558586edd339093455d0933e358c
fiki <fiki@pop-os.localdomain>
2021-07-26 12:48:57 +0700

    add starter page

--- lib/pages/onBoarding_page.dart
+++ lib/pages/onBoarding_page.dart
@@ -1,13 +1,87 @@
 import 'package:flutter/material.dart';
-
+import 'package:future_job/theme.dart';
 
 class OnBoardingPage extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Text('onBoarding page'),
+        child: Stack(
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                  image: DecorationImage(
+                      fit: BoxFit.cover,
+                      image: AssetImage(
+                        'assets/images/onboarding.png',
+                      ))),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 50, right: 30, left: 30),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Build Your Next\nFuture Career Like\na Master',
+                    style: whiteTextColor.copyWith(fontSize: 32),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Text(
+                    '18,000 jobs available',
+                    style: whiteTextColor.copyWith(
+                        fontSize: 14, fontWeight: light),
+                  ),
+                  Center(
+                    child: Column(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(top: 280),
+                          child: Container(
+                            width: 200,
+                            height: 45,
+                            child: TextButton(
+                              onPressed: () {},
+                              child: Text(
+                                "Get Started",
+                                style: purpleTextStyle.copyWith(
+                                  fontWeight: medium
+                                ),
+                              ),
+                              style: TextButton.styleFrom(
+                                  backgroundColor: whiteColor,
+                                  shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(30))),
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Container(
+                          width: 200,
+                          height: 45,
+                          child: OutlinedButton(
+                              onPressed: () {},
+                              style: OutlinedButton.styleFrom(
+                                  side: BorderSide(color: whiteColor),
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(30),
+                                  )),
+                              child: Text(
+                                "Sign In",
+                                style: whiteTextColor,
+                              )),
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
